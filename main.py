b5fa17b310b328cb9d3559aa9ee7b03821c2c29f
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-04-21 18:22:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Determine which player has the "x" pieces.
+# "x" pieces go first.
+# Display game menu. 
+# Game menu contents:
+    # Num of rows/cols.
+    # Powerups per turn.
+    # Num of pieces placed per turn.
+    # Default rulesets are displayed.
+# Game procedure:
+    # Players alternate turns after x's go first.
+    # Players place their piece(s).
+    # If player wins during turn break and show win screen.
+# Display updated gameboard for every piece placed.
+# 
+# Then, show game recap and "Play Again?" prompt.
+# Powerup implementation.
+# Win condition implementation.
+
---
+++ scratch.md
@@ -0,0 +1,6 @@
+# This is the command to convert tex to pdf
+
+```bash
+pandoc sophia_python_project.tex --from latex --to do
+cx > test.docx
+```
