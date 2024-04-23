b247c683ee10fd4c9e6eac55987a592dedcfe630
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-04-22 20:57:40 -0400

    why the jank is necesarry

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 import math
-# Powerup implementation.
-    # Powerups have a grid position and an activation pattern
-    # Individual types of powerups are subclasses of a baseclass Powerup that has members: grid position and activation pattern. Each powerup has its own pattern. The patterns are: "/", "\", "|", and "-"
-    # Powerups determine their poition pseudo-randomly. And, they check if there are spots available before they are placed. They check to see if they overlap other pieces or not before placed.
-    # Powerups are triggered when a players places their piece ontop of it. Their pieces will not be destroyed, but the opposite type of pieces will be.
-    # Optionally, the players can decide to enable a special rule that makes powerup activations destory both types of piecees and other powerups caught in the activation zone.
-    # could add a red trail of the same powerup type along the path of the activation to visually show where the activation happened. This activation should be removed after next piece is placed. The smoke would also have to not hit a certain player as well. 
-
-# TODO Powerups Animation
-
-# TODO Powerups Activation
-
-# TODO Game Logic 
 
 # TODO Grid Display
 # ANSI escape codes for text colors
@@ -23,7 +10,9 @@ PINK = "\x1b[38;5;205m"
 CYAN = "\x1b[36m"
 END = "\033[0m"  # Reset color to default
 
-# Create empty grid.
+xs_placed = 0
+os_placed = 0 
+
 
 pieces = [" . ", " X ", " O ", YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END]
 
@@ -53,9 +42,6 @@ pieces_per_turn = pieces_to_place_per_player_turn()
 
 # Nested loop comprehension to create the 2D matrix for the grid.
 canonical_grid_state = [[pieces[0] for _ in range(n)] for _ in range(n)]
-
-canonical_grid_state[0][0] = pieces[4]
-canonical_grid_state[n-1][n-1] = pieces[4]
 
 # Display the grid.
 def display_grid(grid_state):
@@ -91,10 +77,30 @@ def alternate_turns():
     else:
         return pieces[1]  # Return the piece for the new turn
 
+def current_player_turn():
+    global turn_switch
+    if turn_switch:
+        return "X"
+    else:
+        return "O"
+
+
+
 # TODO Modify grid State
 def modify_grid(alpha_index, numeral_index, grid, piece):
     # Add overlap checks
     grid[alphabet.index(alpha_index)][numeral_index - 1] = piece
+
+    global xs_placed
+    global os_placed
+    if piece == pieces[1]:
+        xs_placed = xs_placed + 1
+    else:
+        os_placed = os_placed + 1
+
+
+
+status_message = ""
 
 
 while True:
@@ -102,39 +108,50 @@ while True:
     
     while True:
         # ANSI code to clear the screen.
+        #print('\033[H\033[J')
+
+        scroll_screen()
+        display_grid(canonical_grid_state)
+        status_color_wrap = "{}{}{}".format(YELLOW, status_message, END)
+        print(status_color_wrap)
+        status_message = ""
 
         #TODO Implement bounds checking.
         valid_alpha_chars = alphabet[:n]
-        location = (input("Place your piece. \n"))
+        hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(current_player_turn(), xs_placed, os_placed)
+        location = input(hud_text)
 
         location = location.split()
 
         l_len = len(location)
 
         if l_len < 2:
-            print("Not enough arguments", "For example: A 1")
+            status_message = "Not enough arguments. For example: A 1"
             continue
         elif l_len > 2:
-            print("Too many arguments", "For example: A 1")
+            status_message = "Too many arguments. For example: A 1"
             continue
 
         alpha_val = location[0]
         numeral_val = location[1]
 
         if not alpha_val in valid_alpha_chars:
-            print("Not a valid alpha input", "For example: A 1")
+            status_message = "Not a valid alpha input. For example: A 1"
             continue
 
-        numeral_val = int (numeral_val)
+        try:
+            numeral_val = int (numeral_val)
+        except:
+            status_message = "the second value should be a number. For example: A 1"
+            continue
         
         if  numeral_val < 1 or numeral_val > n:
-            print("Number should be between 1 and {}".format(n), "val:", numeral_val)
+            status_message = "Number should be between 1 and {}. val: {}.".format(n, numeral_val)
             continue
 
-        modify_grid(alpha_val, numeral_val,  canonical_grid_state, alternate_turns())
+        if canonical_grid_state[alphabet.index(alpha_val)][numeral_val-1] == pieces[1] or canonical_grid_state[alphabet.index(alpha_val)][numeral_val-1] == pieces[2]:
+            continue
+        else:   
+            modify_grid(alpha_val, numeral_val,  canonical_grid_state, alternate_turns())
 
         win_check(canonical_grid_state)
-
-        print('\033[H\033[J')
-        scroll_screen()
-        display_grid(canonical_grid_state)
