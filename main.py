d63f8a4db16b6ecbb9b9909d2cfd4228dca1311f
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-04-23 18:25:49 -0400

    figure out how to map powerups

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import math
+import random
+
+#Debug Mode
+debug = False
+
+def debug_mode_toggle():
+    global debug
+
+    debug = not debug
+
+#Enable debug mode here
+#debug_mode_toggle()
 
 # TODO Grid Display
 # ANSI escape codes for text colors
@@ -13,11 +25,43 @@ END = "\033[0m"  # Reset color to default
 xs_placed = 0
 os_placed = 0 
 
+def current_player_turn():
+    global turn_switch
+    if turn_switch:
+        return "{}".format(pieces[2])
+    else:
+        return "{}".format(pieces[1])
+
+def modify_grid(alpha_index, numeral_index, piece):
+    global canonical_grid_state
+    canonical_grid_state[alpha_index][numeral_index-1] = piece
+
+    global xs_placed
+    global os_placed
+
+    if piece == pieces[1]:
+        xs_placed += 1
+    elif piece == pieces[2]:
+        os_placed += 1
+
+    powerup_check_clean_piece = piece.rstrip()
+
+    powerup_check_clean_piece = " {} ".format(piece)
+
+    powerup_logic(alpha_index, numeral_index, powerup_check_clean_piece)
+
+    
+
+    return canonical_grid_state
+
+
 pieces = [" . ", " X ", " O ", YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END]
 
 pieces_placed_on_this_player_turn = 0
 
 while True:
+    print('\033[H\033[J')
+
     # Num of rows/cols.
     try:
         n = int(input("Enter the number of rows and columns desired. Must be a value between 3-52\n"))
@@ -27,13 +71,15 @@ while True:
         print()
         break
     
-pieces_per_turn =  math.floor(math.log(n**2,5))
+pieces_per_turn = math.floor(math.log(n**2,3))
 
 # Nested loop comprehension to create the 2D matrix for the grid.
 canonical_grid_state = [[pieces[0] for _ in range(n)] for _ in range(n)]
 
 # Display the grid.
 def display_grid(grid_state):
+    print("Pieces to place per player turn", pieces_per_turn)
+    print()
     for i in range(n):
         # Print number prefix per row.
         print("{}".format(alphabet[i]), end = "  ")
@@ -69,30 +115,38 @@ def alternate_turns():
     else:
         return pieces[1]  
 
-def current_player_turn():
-    global turn_switch
-    if turn_switch:
-        return "{}".format(pieces[2])
-    else:
-        return "{}".format(pieces[1])
-# TODO Modify grid State
-def modify_grid(alpha_index, numeral_index, grid, piece):
-    # Add overlap checks
-    grid[alphabet.index(alpha_index)][numeral_index - 1] = piece
+def draw():
+    print("Draw Game")
+    exit()
 
-    global xs_placed
-    global os_placed
+def ugly_powerup_placement_checks():
+    global canonical_grid_state
 
-    if piece == pieces[1]:
-        xs_placed += 1
-    else:
-        os_placed += 1
+    try_count = 0
+
+    while True:
+        
+        powerup_numeral_pos = random.randint(0, n-1)
+        powerup_alpha_pos =  random.randint(0, n-1)
+
+        random_powerup = pieces[random.randint(3, 6)]
+
+        if canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] != pieces[1] and canonical_grid_state[powerup_alpha_pos-1][powerup_numeral_pos] != pieces[2] and canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] == pieces[0] or canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] == pieces[3] or canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] == pieces[4] or canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] == pieces[5] or canonical_grid_state[powerup_alpha_pos][powerup_numeral_pos] == pieces[6]:
+            canonical_grid_state = modify_grid(powerup_alpha_pos, powerup_numeral_pos+1, random_powerup)
+            
+            break
+        elif try_count >= n**3:
+            draw()
+        elif canonical_grid_state[powerup_alpha_pos-1][powerup_numeral_pos-1] != pieces[0]:
+            try_count += 1
+        else:
+            continue
+            
 
 status_message = ""
 
 def win():
     lock_game_state()
-
 
 def horizontal_check(grid):
     for i in range(n):
@@ -151,18 +205,15 @@ def diagonal_up_check(grid, current_pieces):
     j = n-1
 
     for i in range(0 , n-1):
-        print(j, "val: j")
         if grid[i][j] == current_pieces:
             dia_up_count += 1
             j -= 1
-
-    print(grid[0][n-1])
-    print(dia_up_count, "count up")
 
     if dia_up_count == n - 1 and grid[n-1][0] == current_pieces:
         win()
 
 def win_check():
+    global canonical_grid_state
     horizontal_check(canonical_grid_state)
 
     vertical_check(canonical_grid_state)
@@ -182,26 +233,43 @@ def display_status_text(message):
 
 def lock_game_state():
 
-    #scroll_screen()
+    scroll_screen()
     display_grid(canonical_grid_state)
     display_status_text("{}'s WIN!".format(current_player_turn()))
 
     exit(0)
 
+def powerup_logic(alpha_val, numeral_val, powerup_type):
+    if powerup_type == " / ":
+        pass
+    if powerup_type == " \\ ":
+        pass
+    if powerup_type == " | ":
+        pass
+    if powerup_type == " - ":
+        pass
+
 while True:
-    # ANSI code to clear the screen.
-    #print('\033[H\033[J')
+    if debug == False:
 
-    game_logic_turn()
+        # ANSI code to clear the screen.
+        print('\033[H\033[J')
+        scroll_screen()
 
-    #scroll_screen()
     display_grid(canonical_grid_state)
+
+    if game_logic_turn() == True:
+        alternate_turns()
+
 
     display_status_text(status_message)
 
     status_message = ""
 
     while True:
+        #Check for Draw
+
+
         #TODO Implement bounds checking.
         valid_alpha_chars = alphabet[:n]
         hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(current_player_turn(), xs_placed, os_placed)
@@ -238,10 +306,13 @@ while True:
         if canonical_grid_state[alphabet.index(alpha_val)][numeral_val-1] == pieces[1] or canonical_grid_state[alphabet.index(alpha_val)][numeral_val-1] == pieces[2]:
             continue
         else:   
-            modify_grid(alpha_val, numeral_val,  canonical_grid_state, current_player_turn())
+            canonical_grid_state = modify_grid(alphabet.index(alpha_val), numeral_val, current_player_turn())
+            status_message = ""
             break
 
     win_check()
 
+
     if pieces_placed_on_this_player_turn >= pieces_per_turn:
         alternate_turns()
+        ugly_powerup_placement_checks()
