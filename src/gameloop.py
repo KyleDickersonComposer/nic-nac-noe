222fe70179a3c209ade170f71cd413893c3ef712
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-04-24 00:01:56 -0400

    starr factor of gameloop

--- src/gameloop.py
+++ src/gameloop.py
@@ -3,6 +3,7 @@ import main
 
 while True:
     # Num of rows/cols.
+    #get n
     try:
         n = int(input("Enter the number of rows and columns desired. Must be a value between 3-52\n"))
     except:
@@ -11,11 +12,14 @@ while True:
         print()
         break
 
+    #debug logic
     if main.debug == False:
         # ANSI code to clear the screen.
         print('\033[H\033[J')
         lib.scroll_screen()
 
+
+    #display first screen
     lib.display_grid(main.grid, main.n, main.alphabet)
 
     lib.display_status_text(main.status_message)
@@ -23,14 +27,12 @@ while True:
     status_message = ""
 
     while True:
+        #check piece location input
         valid_alpha_chars = main.alphabet[:n]
         hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(lib.current_player_turn(main.turn_switch, main.pieces), main.xs_placed, main.os_placed)
         location = input(hud_text)
-
         location = location.split()
-
         l_len = len(location)
-
         if l_len < 2:
             status_message = "Not enough arguments. For example: A 1"
             lib.display_grid(main.grid, main.n, main.alphabet)
@@ -65,6 +67,7 @@ while True:
             lib.display_status_text(status_message)
             continue
 
+        #check for X's and O's overlapping each other
         if main.grid[main.alphabet.index(alpha_val)][numeral_val-1] == main.pieces[1] or main.grid[main.alphabet.index(alpha_val)][numeral_val-1] == main.pieces[2]:
             lib.display_grid(main.grid, main.n, main.alphabet)
             lib.display_status_text(status_message)
@@ -74,8 +77,8 @@ while True:
             status_message = ""
             break
 
+    #game logic
     lib.win_check(main.grid,main.turn_switch, main.n, main.pieces, main.alphabet)
-
     print(main.pieces_placed_on_this_player_turn, "placed", main.pieces_per_turn, "placements per turn")
     if main.pieces_placed_on_this_player_turn >= main.pieces_per_turn:
         lib.alternate_turns(main.turn_switch, main.pieces)
