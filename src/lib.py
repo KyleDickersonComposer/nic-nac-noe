4c70e6dc57fc28f4f0357c70b8ae1841792b931f
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-08-03 22:22:16 -0400

    Ran a formatter on the code.

--- src/lib.py
+++ src/lib.py
@@ -10,24 +10,29 @@ PINK = "\x1b[38;5;205m"
 CYAN = "\x1b[36m"
 END = "\033[0m"  # Reset color to default
 
-#TODO: create the gamestate class.
+# TODO: create the gamestate class.
+
+
 class GameState:
     def __init__(self, _n):
         self.n = _n
         self.xs_placed = 0
         self.os_placed = 0
-        self.pieces_to_place_for_each_player_turn = math.floor(math.log(self.n**2,3))
+        self.pieces_to_place_for_each_player_turn = math.floor(
+            math.log(self.n**2, 3))
         self.pieces_placed_on_this_player_turn = 0
         self.turn_switch = False
         self.status_message = ""
-        self.pieces = [" . ", " X ", " O ", YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END]
+        self.pieces = [" . ", " X ", " O ", YELLOW + " / " + END,
+                       BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END]
         self.grid = [[" . " for _ in range(self.n)] for _ in range(self.n)]
         self.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-        self.sub_set_of_replacedable_pieces = set([" . ", YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END])
+        self.sub_set_of_replacedable_pieces = set(
+            [" . ", YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END])
         self.set_of_x_y = set([" X ", " O "])
-        self.set_of_powerups = set([YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END])
+        self.set_of_powerups = set(
+            [YELLOW + " / " + END, BLUE + " \\ " + END, PINK + " | " + END, CYAN + " - " + END])
         self.powerup = ""
- 
 
     def current_player_turn(self):
         if self.turn_switch:
@@ -37,7 +42,8 @@ class GameState:
 
     def alternate_player_turn(self, _game_state):
         self.turn_switch = not self.turn_switch
-        _game_state.pieces_placed_on_this_player_turn = 0 
+        _game_state.pieces_placed_on_this_player_turn = 0
+
 
 def nested_list_not_cointains_all_elements_of_set(_nested_list, _set):
     for i in _set:
@@ -46,47 +52,53 @@ def nested_list_not_cointains_all_elements_of_set(_nested_list, _set):
     else:
         return False
 
+
 def display_in_gameloop(_game_state):
-#Display grid with hud and status text if exists.
+    # Display grid with hud and status text if exists.
     display_grid(_game_state.grid, _game_state.n, _game_state.alphabet)
     display_status_text(_game_state.status_message)
 
+
 def clean_input(_input_str, _alphabet):
-    #Inputs are valid at this point.
-    #Convert string coords to int.
+    # Inputs are valid at this point.
+    # Convert string coords to int.
     alpha_index = _alphabet.index(_input_str[0])
-    numeral_index = int (_input_str[1])
-    #fix offset
+    numeral_index = int(_input_str[1])
+    # fix offset
     numeral_index -= 1
 
     return (alpha_index, numeral_index)
 
+
 def get_random_powerup_coords(_n):
     alpha_val = random.randint(0, _n-1)
-    numeral_val =  random.randint(0, _n-1)
+    numeral_val = random.randint(0, _n-1)
 
     return (alpha_val, numeral_val)
+
 
 def get_random_powerup(_set_of_powerups):
     powerup = random.choice(list(_set_of_powerups))
 
     return powerup
 
+
 def validate_powerup_placement(_coords, _grid, _sub_set_of_overridable_pieces, _n):
     location = _grid[_coords[0]][_coords[1]]
     reduce = set([])
 
-    #reduce the list
+    # reduce the list
     for i in range(_n):
         for j in range(_n):
             reduce.add(_grid[i][j])
 
-    #check if piece is in sublist and return true
+    # check if piece is in sublist and return true
     if location in _sub_set_of_overridable_pieces:
         return True
 
     else:
         return False
+
 
 def modify_grid(_coords, _piece, _grid, _game_state):
     if _piece == " X ":
@@ -99,30 +111,34 @@ def modify_grid(_coords, _piece, _grid, _game_state):
 
     return _grid
 
+
 def draw_check(_game_state):
     rset = {i for sub_l in _game_state.grid for i in sub_l}
-    
+
     if rset == _game_state.set_of_x_y:
         draw(_game_state)
     else:
         return False
 
+
 def display_grid(grid_state, _n, _alphabet):
     print()
     for i in range(_n):
         # Print number prefix per row.
-        print("{}".format(_alphabet[i]), end = "  ")
+        print("{}".format(_alphabet[i]), end="  ")
         for j in range(0, _n):
-            print(grid_state[i][j], end = "  ")
+            print(grid_state[i][j], end="  ")
 
         print("\n")
-    print("   ", end = "")
+    print("   ", end="")
     for i in range(_n):
-        print("{:2d}".format(i + 1), end = "   ")
+        print("{:2d}".format(i + 1), end="   ")
     print("\n")
+
 
 def clear_screen():
     print('\033[H\033[J')
+
 
 def draw(_game_state):
     clear_screen()
@@ -130,16 +146,18 @@ def draw(_game_state):
     display_status_text("Draw Game")
     exit()
 
+
 def win(_game_state):
     clear_screen()
     display_grid(_game_state.grid, _game_state.n, _game_state.alphabet)
     display_status_text("{}'s WIN!".format(_game_state.current_player_turn()))
     exit(0)
 
+
 def horizontal_win_check(_game_state):
     for i in range(_game_state.n):
         count = 0
-        
+
         for j in range(_game_state.n):
             if _game_state.grid[i][j] == _game_state.current_player_turn():
                 count += 1
@@ -149,13 +167,14 @@ def horizontal_win_check(_game_state):
         if i != _game_state.n-1:
             continue
 
-        else: 
+        else:
             return False
+
 
 def vertical_win_check(_game_state):
     for i in range(_game_state.n):
         count = 0
-        
+
         for j in range(_game_state.n):
             if _game_state.grid[j][i] == _game_state.current_player_turn():
                 count += 1
@@ -166,11 +185,12 @@ def vertical_win_check(_game_state):
         if i != _game_state.n-1:
             continue
 
-        else: 
+        else:
             return False
 
+
 def diagonal_down_win_check(_game_state):
-    dia_down_count = 0 
+    dia_down_count = 0
 
     for i in range(0, _game_state.n-1):
         if _game_state.grid[i][i] == _game_state.current_player_turn():
@@ -179,17 +199,19 @@ def diagonal_down_win_check(_game_state):
     if dia_down_count == _game_state.n - 1 and _game_state.grid[_game_state.n-1][_game_state.n-1] == _game_state.current_player_turn():
         win(_game_state)
 
+
 def diagonal_up_win_check(_game_state):
     dia_up_count = 0
     j = _game_state.n-1
 
-    for i in range(0 , _game_state.n-1):
+    for i in range(0, _game_state.n-1):
         if _game_state.grid[i][j] == _game_state.current_player_turn():
             dia_up_count += 1
             j -= 1
 
     if dia_up_count == _game_state.n-1 and _game_state.grid[_game_state.n-1][0] == _game_state.current_player_turn():
         win(_game_state)
+
 
 def win_check(_game_state):
     horizontal_win_check(_game_state)
@@ -200,9 +222,11 @@ def win_check(_game_state):
 
     diagonal_up_win_check(_game_state)
 
+
 def display_status_text(message):
     status_color_wrap = "{}{}{}".format(YELLOW, message, END)
     print(status_color_wrap)
+
 
 def powerup_activation_logic(_coords, _this_powerup, _n, _game_state):
     # Powerup type: /
@@ -258,27 +282,30 @@ def powerup_activation_logic(_coords, _this_powerup, _n, _game_state):
         for i in range(_game_state.n):
             if _game_state.grid[i][_coords[1]] != _game_state.current_player_turn():
                 _game_state.grid[i][_coords[1]] = _game_state.pieces[0]
-    
+
     # Powerup type: -
     elif _this_powerup == _game_state.pieces[6]:
         for i in range(_game_state.n):
             if _game_state.grid[_coords[0]][i] != _game_state.current_player_turn():
                 _game_state.grid[_coords[0]][i] = _game_state.pieces[0]
 
+
 def piece_is_powerup(_coords, _grid, _set_of_powerups):
-        coord = _grid[_coords[0]][_coords[1]]
-        if coord in _set_of_powerups:
+    coord = _grid[_coords[0]][_coords[1]]
+    if coord in _set_of_powerups:
 
-            return True
+        return True
 
-        else:
-            return False
+    else:
+        return False
+
 
 def get_input_coords(_prompt):
     print(_prompt)
     location = input()
     location = location.split()
     return location
+
 
 def overlap_check(_coords, _game_state: GameState, _grid):
     location = _grid[_coords[0]][_coords[1]]
@@ -289,38 +316,40 @@ def overlap_check(_coords, _game_state: GameState, _grid):
     else:
         return True
 
+
 def valid_input(_coords, _alphabet, _n, _game_state):
     l_len = len(_coords)
     if l_len < 2:
         _game_state.status_message = "Not enough arguments. For example: A 1"
         return False
- 
+
     elif l_len > 2:
         _game_state.status_message = "Too many arguments. For example: A 1"
         return False
- 
 
     valid_alpha_chars = _alphabet[:_n]
     alpha_val = _coords[0]
     numeral_val = _coords[1]
 
-    if not alpha_val in valid_alpha_chars:
+    if alpha_val not in valid_alpha_chars:
         _game_state.status_message = "Not a valid alpha input. For example: A 1"
         return False
 
     try:
-        numeral_val = int (numeral_val)
+        numeral_val = int(numeral_val)
     except:
         _game_state.status_message = "The second value should be a number. For example: A 1"
         return False
 
-    if  numeral_val < 1 or numeral_val > _n:
-        _game_state.status_message = "Number should be between 1 and {}. val: {}.".format(_n, numeral_val)
+    if numeral_val < 1 or numeral_val > _n:
+        _game_state.status_message = "Number should be between 1 and {}. val: {}.".format(
+            _n, numeral_val)
         return False
 
     else:
         _game_state.status_message = ""
         return True
+
 
 def validate_n(_n_val, _status_message):
     if 4 <= _n_val <= 52:
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import lib 
+import lib
 
 debug_mode = False
 
@@ -7,7 +7,7 @@ lib.clear_screen()
 while True:
     try:
         n = int(input("Enter a whole number between 4 and 52 to determine grid size. "))
-        
+
         if lib.validate_n(n, "The number must be between 4 and 52"):
             lib.clear_screen()
             break
@@ -20,8 +20,8 @@ game_state = lib.GameState(n)
 lib.display_in_gameloop(game_state)
 
 while True:
-    #If debug mode is true print debug statements will be hidden.
-    if debug_mode == False:
+    # If debug mode is true print debug statements will be hidden.
+    if not debug_mode:
         # ANSI code to clear the screen.
         lib.clear_screen()
 
@@ -29,39 +29,46 @@ while True:
 
     last_grid_state = [row[:] for row in game_state.grid]
 
-    hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(game_state.current_player_turn(), game_state.xs_placed, game_state.os_placed)
+    hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(
+        game_state.current_player_turn(), game_state.xs_placed, game_state.os_placed)
 
     prompt_result = lib.get_input_coords(hud_text)
 
-    is_valid = lib.valid_input(prompt_result, game_state.alphabet, game_state.n, game_state)
+    is_valid = lib.valid_input(
+        prompt_result, game_state.alphabet, game_state.n, game_state)
 
-    if is_valid == True:
+    if is_valid:
         player_input = lib.clean_input(prompt_result, game_state.alphabet)
 
         check = lib.overlap_check(player_input, game_state, game_state.grid)
-        
-        if check == True:
-            game_state.grid = lib.modify_grid(player_input, game_state.current_player_turn(), game_state.grid, game_state)
+        if check:
+            game_state.grid = lib.modify_grid(
+                player_input, game_state.current_player_turn(), game_state.grid, game_state)
             lib.win_check(game_state)
             game_state.pieces_placed_on_this_player_turn += 1
 
             if lib.piece_is_powerup(player_input, last_grid_state, game_state.set_of_powerups) == True:
-                powerup_this_turn = lib.get_random_powerup(game_state.set_of_powerups)
-                lib.powerup_activation_logic(player_input, last_grid_state[player_input[0]][player_input[1]], game_state.n, game_state)
+                powerup_this_turn = lib.get_random_powerup(
+                    game_state.set_of_powerups)
+                lib.powerup_activation_logic(
+                    player_input, last_grid_state[player_input[0]][player_input[1]], game_state.n, game_state)
 
             if game_state.pieces_placed_on_this_player_turn == game_state.pieces_to_place_for_each_player_turn:
                 while True:
-                    #Place a piece
+                    # Place a piece
                     try_coords = lib.get_random_powerup_coords(game_state.n)
 
-                    #broken Coords dont overwrite a X or Y
-                    is_valid = lib.validate_powerup_placement(try_coords, game_state.grid, game_state.sub_set_of_replacedable_pieces, game_state.n) 
-                    
-                    if is_valid == True:
-                        powerup_this_turn = lib.get_random_powerup(game_state.set_of_powerups)
+                    # broken Coords dont overwrite a X or Y
+                    is_valid = lib.validate_powerup_placement(
+                        try_coords, game_state.grid, game_state.sub_set_of_replacedable_pieces, game_state.n)
+
+                    if is_valid:
+                        powerup_this_turn = lib.get_random_powerup(
+                            game_state.set_of_powerups)
 
                         if game_state.pieces_placed_on_this_player_turn == game_state.pieces_to_place_for_each_player_turn:
-                            game_state.grid = lib.modify_grid(try_coords, powerup_this_turn, game_state.grid, game_state)
+                            game_state.grid = lib.modify_grid(
+                                try_coords, powerup_this_turn, game_state.grid, game_state)
                             last_grid_state = game_state.grid
                             game_state.alternate_player_turn(game_state)
                             break
