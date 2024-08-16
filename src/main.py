a542addafab6e909d4c596ad49f1f3391a3f35a2
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2024-08-16 13:37:25 -0400

    Thing

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ while True:
         if lib.validate_n(n, "The number must be between 4 and 52"):
             lib.clear_screen()
             break
-    except:
+    except ValueError:
         lib.clear_screen()
         continue
 
@@ -30,12 +30,14 @@ while True:
     last_grid_state = [row[:] for row in game_state.grid]
 
     hud_text = "{}'s turn. Xs placed: {}. Os placed: {}. \n".format(
-        game_state.current_player_turn(), game_state.xs_placed, game_state.os_placed)
+        game_state.current_player_turn(), game_state.xs_placed, game_state.os_placed
+    )
 
     prompt_result = lib.get_input_coords(hud_text)
 
     is_valid = lib.valid_input(
-        prompt_result, game_state.alphabet, game_state.n, game_state)
+        prompt_result, game_state.alphabet, game_state.n, game_state
+    )
 
     if is_valid:
         player_input = lib.clean_input(prompt_result, game_state.alphabet)
@@ -43,32 +45,56 @@ while True:
         check = lib.overlap_check(player_input, game_state, game_state.grid)
         if check:
             game_state.grid = lib.modify_grid(
-                player_input, game_state.current_player_turn(), game_state.grid, game_state)
+                player_input,
+                game_state.current_player_turn(),
+                game_state.grid,
+                game_state,
+            )
             lib.win_check(game_state)
             game_state.pieces_placed_on_this_player_turn += 1
 
-            if lib.piece_is_powerup(player_input, last_grid_state, game_state.set_of_powerups) == True:
-                powerup_this_turn = lib.get_random_powerup(
-                    game_state.set_of_powerups)
+            if lib.piece_is_powerup(
+                player_input, last_grid_state, game_state.set_of_powerups
+            ):
+                powerup_this_turn = lib.get_random_powerup(game_state.set_of_powerups)
                 lib.powerup_activation_logic(
-                    player_input, last_grid_state[player_input[0]][player_input[1]], game_state.n, game_state)
+                    player_input,
+                    last_grid_state[player_input[0]][player_input[1]],
+                    game_state.n,
+                    game_state,
+                )
 
-            if game_state.pieces_placed_on_this_player_turn == game_state.pieces_to_place_for_each_player_turn:
+            if (
+                game_state.pieces_placed_on_this_player_turn
+                == game_state.pieces_to_place_for_each_player_turn
+            ):
                 while True:
                     # Place a piece
                     try_coords = lib.get_random_powerup_coords(game_state.n)
 
                     # broken Coords dont overwrite a X or Y
                     is_valid = lib.validate_powerup_placement(
-                        try_coords, game_state.grid, game_state.sub_set_of_replacedable_pieces, game_state.n)
+                        try_coords,
+                        game_state.grid,
+                        game_state.sub_set_of_replacedable_pieces,
+                        game_state.n,
+                    )
 
                     if is_valid:
                         powerup_this_turn = lib.get_random_powerup(
-                            game_state.set_of_powerups)
+                            game_state.set_of_powerups
+                        )
 
-                        if game_state.pieces_placed_on_this_player_turn == game_state.pieces_to_place_for_each_player_turn:
+                        if (
+                            game_state.pieces_placed_on_this_player_turn
+                            == game_state.pieces_to_place_for_each_player_turn
+                        ):
                             game_state.grid = lib.modify_grid(
-                                try_coords, powerup_this_turn, game_state.grid, game_state)
+                                try_coords,
+                                powerup_this_turn,
+                                game_state.grid,
+                                game_state,
+                            )
                             last_grid_state = game_state.grid
                             game_state.alternate_player_turn(game_state)
                             break
